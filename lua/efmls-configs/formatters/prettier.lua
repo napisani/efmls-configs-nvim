11867dde6ec20e74027a0d5f6253bc35d43a8c32
Arnold Chand <creativenull@outlook.com>
2025-11-02 11:31:51 -0400

    fix(prettier,prettierd): use config precedence, rm opts passed from lsp

--- lua/efmls-configs/formatters/prettier.lua
+++ lua/efmls-configs/formatters/prettier.lua
@@ -6,8 +6,7 @@ local fs = require('efmls-configs.fs')
 
 local formatter = 'prettier'
 local command = string.format(
-  "%s --stdin --stdin-filepath '${INPUT}' ${--range-start:charStart} "
-    .. '${--range-end:charEnd} ${--tab-width:tabSize} ${--use-tabs:!insertSpaces}',
+  "%s --stdin --stdin-filepath ${--range-start:charStart} ${--range-end:charEnd} --config-precedence prefer-file '${INPUT}'",
   fs.executable(formatter, fs.Scope.NODE)
 )
 
--- lua/efmls-configs/formatters/prettier_d.lua
+++ lua/efmls-configs/formatters/prettier_d.lua
@@ -6,8 +6,7 @@ local fs = require('efmls-configs.fs')
 
 local formatter = 'prettierd'
 local command = string.format(
-  "%s '${INPUT}' ${--range-start=charStart} ${--range-end=charEnd} "
-    .. '${--tab-width=tabSize} ${--use-tabs=!insertSpaces}',
+  "%s ${--range-start=charStart} ${--range-end=charEnd} --config-precedence=prefer-file '${INPUT}'",
   fs.executable(formatter, fs.Scope.NODE)
 )
 
